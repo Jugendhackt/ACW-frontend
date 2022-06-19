bb87d6fc7ee2b6456c11c2485d91a7c83a472c29
Mitja Galkin <mitja.galkin@altow.de>
2022-06-19 09:36:52 +0200

    add fixes.

--- ACW/ACW.js
+++ ACW/ACW.js
@@ -1,4 +1,4 @@
-function start_observer(){
+/*function start_observer(){
   const targetNode = document.getElementsByClassName("Post")[0].parentElement;
   const config = { attributes: true, childList: true,};
   const callback = function(mutationList, observer) {
@@ -6,38 +6,58 @@ function start_observer(){
           if (mutation.type === 'childList') {
             elements = document.getElementsByClassName("Post");
             for(i in elements){
-              setTimeout(handleElement(mutation.target), 100);
+              console.log(i.attributes)
             }
           }
       }
   };
   const observer = new MutationObserver(callback);
   observer.observe(targetNode, config);
-}
+}*/
 
 function handleElement(element){
   try {
-    if (!(element.parentElement.classList.contains("DoNotWatch"))){
-    newDiv = document.createElement("div");
-    element.parentElement.appendChild(newDiv);
-    newDiv.setAttribute("class", "PostOverlay DoNotWatch");
-    newDiv.setAttribute("style", "border-style: solid; border-color: #898989; border-width: 1px; border-radius: 2px;");
-    newDiv.appendChild(element);
-    element.setAttribute("style", "filter: blur(10px); pointer-events:none; padding-bot:-10px")
-  }
-  } catch (e) {
+    if (!(element.parentElement.classList.contains("DoNotWatch"))) {
+      newDiv = document.createElement("div");
+      element.parentElement.appendChild(newDiv);
+      newDiv.setAttribute("class", "PostOverlay DoNotWatch");
+      newDiv.setAttribute("style", "border-style: solid; border-color: #898989; border-width: 1px; border-radius: 2px; display: flex; justify-content: center; align-items: center;");
+      newDiv.appendChild(element);
 
-  } finally {
 
-  }
+      newDiv.onmouseover = function() {
+        testText = document.createElement("h6");
+        testText.innerHTML = "This element was blurred by ACW.";
+        testText.setAttribute("style", "position: absolute; z-index: 999; flex: 0 0 120px; color: white; font-size: 35px; -webkit-text-stroke-width: 1px; -webkit-text-stroke-color: black;");
+        element.parentElement.appendChild(testText);
+      }
+
+      newDiv.onmouseout = function() {          
+        try {  
+          element.parentElement.removeChild(testText);
+        } catch(e) { }
+      }
+
+      element.setAttribute("style", "filter: blur(10px); pointer-events:none; padding-bot:-10px")
+
+    }
+  } catch (e) {} finally {}
 };
 
-function get_elements_reddit(){
-  start_observer()
+function doBlur() {
+  blurElements();
+}
+
+function blurElements() {
   elements = document.getElementsByClassName("Post");
   for(i in elements){
     setTimeout(handleElement(elements[i]), 100);
   }
 }
 
-addEventListener("DOMContendLoaded",get_elements_reddit());
+function ACW(){
+  blurElements();
+  setInterval(doBlur, 1000);
+}
+
+addEventListener("DOMContentLoaded", ACW());
