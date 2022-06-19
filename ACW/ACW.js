2a466910f824584572a998312ebed826f0d8961d
Mitja Galkin <mitja.galkin@altow.de>
2022-06-19 09:37:30 +0200

    add testing getcategories

--- ACW/ACW.js
+++ ACW/ACW.js
@@ -55,6 +55,25 @@ function blurElements() {
   }
 }
 
+
+function getCategories() {
+  // testing.
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://acw.ipv6.church/");
+
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onload = () => console.log(xhr.responseText);
+
+  let data = `{
+    "Id": 78912,
+    "Customer": "Jason Sweet",
+  }`;
+
+  xhr.send(data);
+}
+
 function ACW(){
   blurElements();
   setInterval(doBlur, 1000);
