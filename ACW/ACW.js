a01de3e5e4fd02a749e016104423a6db222d173b
frieda <without.frieda@posteo.de>
2022-06-19 11:27:35 +0200

    I'm happy it works fine fixed some weird bugs and add blur on website load

--- ACW/ACW.js
+++ ACW/ACW.js
@@ -1,20 +1,4 @@
-/*function start_observer(){
-  const targetNode = document.getElementsByClassName("Post")[0].parentElement;
-  const config = { attributes: true, childList: true,};
-  const callback = function(mutationList, observer) {
-      for(const mutation of mutationList) {
-          if (mutation.type === 'childList') {
-            elements = document.getElementsByClassName("Post");
-            for(i in elements){
-              console.log(i.attributes)
-            }
-          }
-      }
-  };
-  const observer = new MutationObserver(callback);
-  observer.observe(targetNode, config);
-}*/
-
+document.body.setAttribute("style", "filter:blur(20px);")
 function handleElement(element){
   try {
     if (!(element.parentElement.classList.contains("DoNotWatch"))) {
@@ -23,35 +7,28 @@ function handleElement(element){
       newDiv.setAttribute("class", "PostOverlay DoNotWatch");
       newDiv.setAttribute("style", "border-style: solid; border-color: #898989; border-width: 1px; border-radius: 2px; display: flex; justify-content: center; align-items: center;");
       newDiv.appendChild(element);
-
-
-      newDiv.onmouseover = function() {
-        testText = document.createElement("h6");
-        testText.innerHTML = "This element was blurred by ACW.";
-        testText.setAttribute("style", "position: absolute; z-index: 999; flex: 0 0 120px; color: white; font-size: 35px; -webkit-text-stroke-width: 1px; -webkit-text-stroke-color: black;");
-        element.parentElement.appendChild(testText);
+        warningText = document.createElement("h6");
+        warningText.innerHTML = "ACW: loading()\nClick to display";
+        warningText.innerHTML = "ACW: "+"coming soon<br>Click to display";
+        warningText.setAttribute("style", "position: absolute; z-index: 999; flex: 0 0 120px; color: white; font-size: 35px; -webkit-text-stroke-width: 1px; -webkit-text-stroke-color: black;");
+        warningText.setAttribute("class", "textToDelete")
+        element.parentElement.appendChild(warningText);
+      element.setAttribute("style", "filter: blur(10px); pointer-events:none; padding-bot:-10px");
+      newDiv.onclick = function() {
+        console.log("miep");
+          element.parentElement.removeAttribute("style")
+          element.removeAttribute("style")
+          element.parentElement.getElementsByClassName("textToDelete")[0].remove()
       }
-
-      newDiv.onmouseout = function() {          
-        try {  
-          element.parentElement.removeChild(testText);
-        } catch(e) { }
-      }
-
-      element.setAttribute("style", "filter: blur(10px); pointer-events:none; padding-bot:-10px")
-
     }
   } catch (e) {} finally {}
 };
 
-function doBlur() {
-  blurElements();
-}
 
 function blurElements() {
   elements = document.getElementsByClassName("Post");
   for(i in elements){
-    setTimeout(handleElement(elements[i]), 100);
+    handleElement(elements[i])
   }
 }
 
@@ -75,8 +52,14 @@ function getCategories() {
 }
 
 function ACW(){
-  blurElements();
-  setInterval(doBlur, 1000);
+  setTimeout(function (){
+    blurElements()
+    setInterval(blurElements, 1000);
+  }, 1000);
+  setTimeout(function (){
+    document.body.removeAttribute("style")
+  }, 3000);
+
 }
 
 addEventListener("DOMContentLoaded", ACW());
